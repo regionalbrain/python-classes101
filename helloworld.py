8d01099b9fca90b63f0dad29e4d985687be3226a
regionalbrain <54288617+regionalbrain@users.noreply.github.com>
2022-06-18 08:50:14 +0300

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,30 @@
+print("hello world")
+print(2**2)
+print(600*117)
+print(5/2)
+print(5//2)
+print(5%2)
+print("what'sup homie")
+#variables 
+x=5 
+y="john"
+print(x)
+print(y)
+
+z = str(3)
+print(z)
+s = float(7)
+print(s)
+#you can get the data type using print(type(?))
+print(type(x))
+print(type(y))
+#variables are case sensitive e.g A and a are two differnt variables. Also, the variable in caps will always overwrite the small letter variable 
+#different ways of namimg variables 
+myVariableName=23
+MyVariableName=25
+my_variable_name=26
+price=10
+print(price)
+print(4**2)
+k=15+4**2*3
+print(k)
